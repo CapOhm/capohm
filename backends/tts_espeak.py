6e7109a8beca63804346d223e5e18d872fa93001
CapOhm <smokey.sky@gmail.com>
2026-05-30 22:04:33 +0200

    Add switchable espeak TTS backend

---
+++ backends/tts_espeak.py
@@ -0,0 +1,22 @@
+import subprocess
+
+
+class TTS:
+    def __init__(self, config):
+        self.voice = config.get("espeak_voice", "en+m3")
+        self.speed = str(config.get("espeak_speed", 140))
+        self.pitch = str(config.get("espeak_pitch", 45))
+        self.process = None
+
+    def speak(self, text: str) -> None:
+        self.stop()
+        self.process = subprocess.Popen(
+            ["espeak", "-v", self.voice, "-s", self.speed, "-p", self.pitch, text],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+    def stop(self) -> None:
+        if self.process and self.process.poll() is None:
+            self.process.terminate()
+            self.process = None
